8b045f78f04a07ed5865be5ce7fb04e7a8f822ef
Federico Miras <miras.federico@gmail.com>
2023-07-14 20:46:12 -0300

    make camera center to GLTF box center

--- src/gltf.jsx
+++ src/gltf.jsx
@@ -1,23 +1,39 @@
-import { Suspense } from 'react'
+import * as THREE from 'three'
+import { useEffect } from 'react'
 import { withResizeDetector } from 'react-resize-detector'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useThree } from '@react-three/fiber'
 import { OrbitControls, useGLTF } from '@react-three/drei'
 
 function GLTF({ src }) {
   const { scene } = useGLTF(src)
-  return <primitive object={scene} />
+  const { camera } = useThree()
+
+  const box = new THREE.Box3().setFromObject(scene)
+  const size = box.getSize(new THREE.Vector3())
+  const center = box.getCenter(new THREE.Vector3())
+
+  useEffect(() => {
+    camera.position.set(size.x, size.y, size.z)
+    camera.lookAt(center)
+  }, [camera, scene.position])
+
+  return (
+    <mesh>
+      <primitive object={scene} />
+      <OrbitControls target={center} />
+    </mesh>
+  )
 }
 
 export function GLTFBlock(props) {
   const src = props.children[0]
   return (
-    <Canvas pixelRatio={[1, 1]} camera={{ position: [-100, 60, 100], fov: 60 }}>
-      <ambientLight intensity={1} />
-      <Suspense fallback={null}>
+    <div className="inkdrop-gltf-canvas" style={{ height: '20vh', width: '100%' }}>
+      <Canvas flat linear>
+        <ambientLight intensity={0.5} />
         <GLTF src={src} />
-      </Suspense>
-      <OrbitControls />
-    </Canvas>
+      </Canvas>
+    </div>
   )
 }
 
